86d9a423b9e13608240a8b771d37c9b8258a1c3a
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2018-03-21 10:15:31 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'blogs';
+  blogs = [
+    {title: 'Title'},
+    {title: 'Title2'},
+    {title: 'Title3'},
+    {title: 'Title4'},
+  ];
 }
